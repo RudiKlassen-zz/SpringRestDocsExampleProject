f3259619fdc637b5f700cef45f2ca9ec2f38b1ec
Rudi <rudolfklassen2@gmail.com>
2019-02-25 17:35:32 +0100

    move dir

---
+++ src/main/java/com/springrestexample/encodingproblem/EncodingproblemApplication.java
@@ -0,0 +1,13 @@
+package com.springrestexample.encodingproblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncodingproblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncodingproblemApplication.class, args);
+	}
+
+}
